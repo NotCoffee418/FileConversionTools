a84cceb8c3453e3ae5693014c9b8922a4c321640
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2022-12-06 11:53:47 +0100

    VS Files + operation setup

---
+++ FileConversionTools/Program.cs
@@ -0,0 +1,8 @@
+﻿using CSharpScriptOperations;
+
+OperationManager.RegisterOperationsBulk(
+    new List<Type>() {
+        //typeof(TwoPlusTwo),
+    }
+);
+await OperationManager.StartListeningAsync();
